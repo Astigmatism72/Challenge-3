e4f6493c2a2497e7a3ea8c3ef03aabb4051624d5
Duncan <dcduncandc@gmail.com>
2022-06-08 23:52:18 -0400

    page opens with README

--- Develop/script.js
+++ Develop/script.js
@@ -113,3 +113,5 @@ var passwordText = document.querySelector("#password");
 passwordText.value = password;
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// comment for safety
